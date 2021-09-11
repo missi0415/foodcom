a214eb823257b01a2d63763dde8762cb9699965e
missi0415 <missi0415@gmail.com>
2021-09-12 00:02:52 +0900

    [fix]chat機能スクロール機能導入

--- nuxt_twitter/front/pages/rooms/_id.vue
+++ nuxt_twitter/front/pages/rooms/_id.vue
@@ -1,66 +1,99 @@
 <template>
   <layout-main #layout-main> <!--eslint-disable-line-->
-    <v-container>
-      <v-row justify="space-around">
-        <v-card>
-          <v-app-bar
-            flat
-            color="rgba(0, 0, 0, 0)"
-          >
-            <v-btn
-              icon
-              large
-              class="mr-5"
-              @click="pageBack"
-            >
-              <v-icon>mdi-arrow-left</v-icon>
-            </v-btn>
-            <v-toolbar-title>
-              {{ user.name }}
-            </v-toolbar-title>
-          </v-app-bar>
-          <v-divider />
-          <v-card-text>
-            相手user{{ user }}
-          </v-card-text>
-          <v-card-text>
-            自分currentuser {{ currentUser }}
-          </v-card-text>
-          <v-card-text>
-            firestoredata {{ messages }}
-          </v-card-text>
-          <v-card-text>
-            chats {{ chats }}
-          </v-card-text>
-          <v-card-text>
-            channel_id {{ channel_id }}
-          </v-card-text>
-          <v-list three-line>
-            <v-list-item
-              v-for="(item, i) in chats"
-              :key="i"
-            >
-              <v-list-item-content>
-                <v-list-item-title v-text="item.name"></v-list-item-title>
-                <v-list-item-subtitle v-text="item.message"></v-list-item-subtitle>
-              </v-list-item-content>
-            </v-list-item>
-          </v-list>
-          <v-form>
-            <v-text-field
-              v-model="message"
-              label="メッセージを入力"
-            >
-            </v-text-field>
-            <v-btn
-              @click="onSubmit"
-            >
-            送信
-            </v-btn>
-          </v-form>
-        </v-card>
-      </v-row>
-    </v-container>
+    <v-card
+      class="mx-auto"
+    >
+      <v-app-bar
+        flat
+        color="rgba(0, 0, 0, 0)"
+      >
+        <v-btn
+          icon
+          large
+          class="mr-5"
+          @click="pageBack"
+        >
+          <v-icon>mdi-arrow-left</v-icon>
+        </v-btn>
+        <v-toolbar-title>
+          {{ user.name }}
+        </v-toolbar-title>
+      </v-app-bar>
+      <v-divider />
+      <!-- チャット画面 -->
+      <v-list
+        class="overflow-y-auto mx-auto "
+        width="100%"
+        max-height="600"
+        max-width="1000"
+        id="chat-height"
+      >
+      <v-list-item
+        v-for="(msg, i) in chats"
+        :key="i"
+      >
+          <v-col v-if="msg.userId != currentUser.uid">
+            <div class="balloon_l">
+              <div class="face_icon">
+                <v-avatar
+                  size="60"
+                >
+                  <img
+                    :src="userAvatar"
+                    contain
+                  >
+                </v-avatar>
+              </div>
+              <p class="says">
+                {{ msg.message }}
+              </p>
+            </div>
+          </v-col>
+          <v-col v-else>
+            <div class="balloon_r">
+              <div class="face_icon">
+                <v-avatar
+                  size="60"
+                  contain
+                >
+                  <img
+                    :src="currentUserAvatar"
+                  >
+                </v-avatar>
+              </div>
+              <p class="says-current">
+                {{ msg.message }}
+                {{ msg.createdAt }}
+              </p>
+            </div>
+          </v-col>
+        </v-list-item>
+      </v-list>
+      <!-- form送信部分 -->
+      <v-divider
+        id="scroll-inner"
+      />
+      <div class="ma-0 pa-0 d-flex flex-row align-baseline">
+        <v-text-field
+          v-model="message"
+          label="メッセージを入力"
+          type="text"
+          outlined
+          dense
+          rounded
+          class="mt-6 pa-2"
+        />
+        <v-btn
+          class="ma-2 pa-2"
+          outlined
+          rounded
+          color="myblue"
+          @click="onSubmit"
+        >
+          送信
+        </v-btn>
+      </div>
+    </v-card>
   </layout-main>
 </template>
 
@@ -80,7 +113,9 @@ export default {
       message: '',
       messages: [],
       chats: [],
-      channel_id: ''
+      channel_id: '',
+      userAvatar: '',
+      currentUserAvatar: ''
     }
   },
   computed: {
@@ -88,18 +123,16 @@ export default {
       currentUser: 'auth/data',
       currentUserId: 'auth/currentUserId',
       isAuthenticated: 'auth/isAuthenticated'
-      // chats: 'chats/chats'
     })
   },
   mounted () {
-    this.fetchUser()
     setTimeout(() => {
-      this.setChannelId()
-      this.fetchMessage()
-    }, 300)
+      this.fetchUser()
+    }, 100)
   },
   methods: {
     async fetchUser () {
+      this.currentUserAvatar = this.currentUser.avatar.url
       const url = `/api/v1/users/${this.$route.params.id}`
       await this.$axios.get(url)
         .then((res) => {
@@ -108,7 +141,10 @@ export default {
           this.user.id = res.data.user.id
           this.user.name = res.data.user.name
           this.user.avatar = res.data.user.avatar.url
+          this.userAvatar = res.data.user.avatar.url
           this.user.introduction = res.data.user.introduction
+          this.setChannelId()
+          this.fetchMessage()
         })
         .catch((err) => {
           // eslint-disable-next-line no-console
@@ -131,21 +167,21 @@ export default {
         .add(chat)
       this.resetForm()
       this.fetchMessage()
-      // this.scrollBottom()
     },
     resetForm () {
       this.message = ''
     },
-    fetchMessage () {
+    async fetchMessage () {
       const chats = []
-      firebase.firestore().collection('rooms').doc(this.channel_id).collection('chats').orderBy('createdAt', 'asc').get()
+      await firebase.firestore().collection('rooms').doc(this.channel_id).collection('chats').orderBy('createdAt', 'asc').get()
         .then((res) => {
-          console.log('res', res)
           res.forEach((doc) => {
             chats.push(doc.data())
             this.chats = chats
-            console.log('doc.data()', doc.data())
           })
+          setTimeout(() => {
+            this.scrollBottom()
+          }, 100)
         })
         .catch((err) => {
           // eslint-disable-next-line no-console
@@ -161,8 +197,90 @@ export default {
       } else {
         this.channel_id = this.currentUser.uid + '-' + this.user.uid
       }
+    },
+    scrollBottom () {
+      console.log('scrollBottom')
+      const chatBack = document.getElementById('chat-height')
+      chatBack.scrollTop = chatBack.scrollHeight
     }
   }
 }
 
 </script>
+<style scoped>
+.balloon_l,
+.balloon_r {
+  margin: 10px 0;
+  display: flex;
+  justify-content: flex-start;
+  align-items: flex-start;
+}
+.balloon_r {
+  justify-content: flex-end;
+}
+.face_icon img {
+  width: 80px;
+  height: auto;
+}
+.balloon_r .face_icon {
+  margin-left: 25px;
+}
+.balloon_l .face_icon {
+  margin-right: 25px;
+}
+.balloon_r .face_icon {
+  order: 2 !important;
+}
+.says {
+  max-width: 300px;
+  display: flex;
+  flex-wrap: wrap;
+  position: relative;
+  padding: 10px;
+  border-radius: 12px;
+  background: #E3F2FD;
+  box-sizing: border-box;
+  margin: 0 !important;
+  line-height: 1.5;
+  /*   align-items: center; */
+}
+.says-current {
+  max-width: 300px;
+  display: flex;
+  flex-wrap: wrap;
+  position: relative;
+  padding: 10px;
+  border-radius: 12px;
+  background: #64B5F6;
+  box-sizing: border-box;
+  margin: 0 !important;
+  line-height: 1.5;
+  /*   align-items: center; */
+}
+.says p {
+  margin: 8px 0 0 !important;
+}
+.says p:first-child {
+  margin-top: 0 !important;
+}
+.says:after {
+  content: "";
+  position: absolute;
+  border: 10px solid transparent;
+  margin-top: -3px;
+}
+.says-current:after {
+  content: "";
+  position: absolute;
+  border: 10px solid transparent;
+  margin-top: -3px;
+}
+.balloon_l .says:after {
+  left: -26px;
+  border-right: 22px solid #E3F2FD;
+}
+.balloon_r .says-current:after {
+  right: -26px;
+  border-left: 22px solid #64B5F6;
+}
+</style>
