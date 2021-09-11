71dbb8247a39e8c61d43bf27f3062cf2ac7cfc70
missi0415 <missi0415@gmail.com>
2021-09-11 11:36:06 +0900

    [add]channel_id一意のものの設定

--- nuxt_twitter/front/pages/rooms/_id.vue
+++ nuxt_twitter/front/pages/rooms/_id.vue
@@ -1,38 +1,55 @@
 <template>
   <layout-main #layout-main> <!--eslint-disable-line-->
-    <v-card
-      outlined
-      elevation="15"
-    >
-    <!-- firebase.firestore()= -->
-      <v-card-text>
-        page account/account.vue
-      </v-card-text>
-      <v-card-text>
-        相手user{{ user }}
-      </v-card-text>
-      <v-card-text>
-        自分currentuser {{ currentUser }}
-      </v-card-text>
-      <v-card-text>
-        firestoredata {{ messages }}
-      </v-card-text>
-      <v-card-text>
-        chats {{ chats }}
-      </v-card-text>
-      <v-form>
-        <v-text-field
-          v-model="message"
-          label="メッセージを入力"
-        >
-        </v-text-field>
-        <v-btn
-          @click="onSubmit"
-        >
-        送信
-        </v-btn>
-      </v-form>
-    </v-card>
+    <v-container>
+      <v-row justify="space-around">
+        <v-card>
+          <v-app-bar
+            flat
+            color="rgba(0, 0, 0, 0)"
+          >
+            <v-btn
+              icon
+              large
+              class="mr-5"
+              @click="pageBack"
+            >
+              <v-icon>mdi-arrow-left</v-icon>
+            </v-btn>
+            <v-toolbar-title>
+              {{ user.name }}
+            </v-toolbar-title>
+          </v-app-bar>
+          <v-divider />
+          <v-card-text>
+            相手user{{ user }}
+          </v-card-text>
+          <v-card-text>
+            自分currentuser {{ currentUser }}
+          </v-card-text>
+          <v-card-text>
+            firestoredata {{ messages }}
+          </v-card-text>
+          <v-card-text>
+            chats {{ chats }}
+          </v-card-text>
+          <v-card-text>
+            channel_id {{ channel_id }}
+          </v-card-text>
+          <v-form>
+            <v-text-field
+              v-model="message"
+              label="メッセージを入力"
+            >
+            </v-text-field>
+            <v-btn
+              @click="onSubmit"
+            >
+            送信
+            </v-btn>
+          </v-form>
+        </v-card>
+      </v-row>
+    </v-container>
   </layout-main>
 </template>
 
@@ -47,11 +64,12 @@ export default {
   },
   data () {
     return {
-      user: { id: '', name: '', avatar: '' },
+      user: { id: '', name: '', avatar: '', introduction: '', uid: '' },
       // rooms: [],
       message: '',
       messages: [],
-      chats: []
+      chats: [],
+      channel_id: ''
     }
   },
   computed: {
@@ -63,19 +81,23 @@ export default {
     })
   },
   mounted () {
+    this.fetchUser()
     setTimeout(() => {
-      this.fetchUser()
       this.fetchMessage()
-    }, 100)
+      this.setChannelId()
+    }, 300)
   },
   methods: {
     async fetchUser () {
       const url = `/api/v1/users/${this.$route.params.id}`
       await this.$axios.get(url)
         .then((res) => {
+          console.log('res', res)
+          this.user.uid = res.data.user.uid
           this.user.id = res.data.user.id
           this.user.name = res.data.user.name
           this.user.avatar = res.data.user.avatar.url
+          this.user.introduction = res.data.user.introduction
         })
         .catch((err) => {
           // eslint-disable-next-line no-console
@@ -118,6 +140,24 @@ export default {
           // eslint-disable-next-line no-console
           console.error(err)
         })
+    },
+    pageBack () {
+      this.$router.go(-1)
+    },
+    setChannelId () {
+      console.log('user.uidが撮ってこれてるか', this.user.uid)
+      console.log('currentUser.uidが撮ってこれてるか', this.currentUser.uid)
+      if (this.user.uid > this.currentUser.uid) {
+        console.log('user.uidがでかい')
+        console.log(this.user.uid)
+        this.channel_id = this.user.uid + '-' + this.currentUser.uid
+        console.log('channel', this.channel_id)
+      } else {
+        console.log('currentUser.uidがでかい')
+        this.channel_id = this.currentUser.uid + '-' + this.user.uid
+        console.log('user.uid', this.user.uid)
+        console.log('channel', this.channel_id)
+      }
     }
   }
 }
